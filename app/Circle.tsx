8cfa99616f815f172757c853700c64df277cd24a
Toufiqh92 <Toufiqh12@gmail.com>
2026-01-15 03:12:55 -0500

    i

--- app/Circle.tsx
+++ app/Circle.tsx
@@ -4,8 +4,7 @@ import { router } from "expo-router";
 export default function Circle() {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Circle This is where you can add your friends while youre out and see your friends Drinking progressions and
-            </Text>
+            <Text>Circle This is where you can add your friends while you are out</Text>
             <Button title="Home" onPress={() => window.location.reload()} />
             <Button title="Add Friends" onPress={() => router.push("/addFriends")} />
         </View>
